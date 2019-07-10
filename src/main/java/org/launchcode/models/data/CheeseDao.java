f75c05cce8bfd57ee7377e6d30521883ebc147cd
John Durham <kilo.kilo.delta8@gmail.com>
2019-07-10 15:08:19 -0400

    updated and corrected issues in daos

--- src/main/java/org/launchcode/models/data/CheeseDao.java
+++ src/main/java/org/launchcode/models/data/CheeseDao.java
@@ -3,12 +3,13 @@ package org.launchcode.models.data;
 import org.launchcode.models.Cheese;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by LaunchCode
  */
 @Repository
-@Transactional  // all methods should be as one transaction
+@Transactional
 public interface CheeseDao extends CrudRepository<Cheese, Integer> {
 }
